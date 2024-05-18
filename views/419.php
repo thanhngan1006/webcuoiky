73ca9f785b4207cca89849796f6b4d524f842c78
Mon <thanhngan10604@gmail.com>
2024-05-19 02:09:52 +0700

    handle invalid link

---
+++ views/419.php
@@ -0,0 +1,8 @@
+<?php require('partials/head.php') ?>
+<div class="flex flex-col items-center justify-center w-screen h-screen">
+  <div class="flex flex-col gap-5 items-center p-3 bg-common-error/10 sm:max-w-[50%] shadow-md rounded-lg">
+    <span class="text-lg font-semibold">Thông báo</span>
+    <span>Liên kết bạn truy cập đã hết hạn, vui lòng liên hệ admin để được cấp liên kết mới</span>
+    <a href="/" class="button-primary-solid-sm">Trang chủ</a>
+  </div>
+</div>
